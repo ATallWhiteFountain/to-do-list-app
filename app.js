47ac89a69f9f436332d9bad3bb866f3493afe389
ATallWhiteFountain <bswatekpost@gmail.com>
2020-08-28 20:25:25 +0200

    MongoDB migration

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 //jshint esversion:6
 
 const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("css"));
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const app = express();
 
-const tasks = [];
-const workTasks = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("css"));
+mongoose.connect("mongodb://localhost:27017/todolist", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({name: "Get enough sleep."});
+const item2 = new Item({name: "Get food and water."});
+const item3 = new Item({name: "Get clean and fragrant."});
+const defaultItems = [item1, item2, item3];
 
 app.get("/", (req, res) => {
+    
     let formattedCurrentDate = date.getDate();    
-    res.render("list", {listTitle: formattedCurrentDate, taskList: tasks});
+    
+    Item.find({}, (err, itemsFound) => {
+        if(err) console.log("Query failed.");
+        else {
+            if(itemsFound.length === 0){
+                Item.insertMany(defaultItems, (err) => {
+                    if(err) console.log("Initial insertion failed.");
+                    else console.log("Initial insertion success.");
+                });
+                res.redirect("/");
+            }
+            else res.render("list", {listTitle: formattedCurrentDate, taskList: itemsFound});
+        }
+    });
 });
 
 app.get("/work", (req, res) => {
@@ -27,14 +49,21 @@ app.get("/about", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    let task = req.body.newTask;
-    if (req.body.list === "Work") {
-        workTasks.push(task);
-        res.redirect("/work");
-    } else {
-        tasks.push(task);
-        res.redirect("/");
-    }
+    let itemName = req.body.newTask;
+    const newItem = new Item({name: itemName});
+    newItem.save();
+    res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, (err, doc) => {
+        if(err) console.log("Removing failed.");
+        else {
+            console.log("Removing succeeded");
+            res.redirect("/");
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
